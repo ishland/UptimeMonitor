9a632dc5d1985183048e845c7c5f1d0c0f3ce09b
TeresaQWQ <yxiao2044@gmail.com>
2018-08-29 02:21:42 +0000

    new file:   web/css/style.css 	new file:   web/index.html 	new file:   web/js/main.js 	new file:   web/pages/minecraft.html

---
+++ web/js/main.js
@@ -0,0 +1,43 @@
+function ajaxPost(uri, data) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("POST", uri, true);
+    xmlhttp.send(data);
+}
+
+function ajaxGet(uri) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", uri, true);
+    xmlhttp.send();
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+}
